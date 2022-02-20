320a5d0dae8ede70fcc20747eca19b5a037ef7cd
Sean Toner <placeoftheway@gmail.com>
2022-02-20 10:21:54 -0500

    Added documentation

--- crossbow/src/libs/meta.rs
+++ crossbow/src/libs/meta.rs
@@ -3,21 +3,35 @@
 
 use chrono::{DateTime, Duration, Utc};
 
+/// new type for an S3 URI
 pub struct S3Path(String);
 
+/// Different states for a test run
 pub enum RunStatus {
-    Pass,    // Test failed
-    Fail,    // Test did not pass assertion(s)
-    Skipped, // Test was not run
-    Error,   // test failed, but not due to assertion
+    /// Test passed all assertions
+    Pass,
+    /// Test failed one or more assertions
+    Fail,    
+    /// Test was not executed
+    Skipped,
+    /// test failed, but not due to assertion
+    Error,
 }
 
+/// Represents relevant data for a test run
 pub struct RunResult {
+    /// The status of the test
     pub status: RunStatus,
+    /// Name of the test
     pub name: String,
+    /// When the test was executed
     pub start_time: DateTime<Utc>,
+    /// When the test completed
     pub end_time: DateTime<Utc>,
+    /// Time test took to run
     pub duration: Duration,
+    /// Message of test failure
     pub failure: Option<String>,
+    /// Where test result is stored
     pub log: S3Path,
 }
--- crossbow/src/main.rs
+++ crossbow/src/main.rs
@@ -1,6 +1,6 @@
-use clap::App;
+use clap::Command;
 
 #[tokio::main]
 async fn main() {
-    App::new("docker demo").about("Build an executable for ");
+    Command::new("docker demo").about("Build an executable for ");
 }
