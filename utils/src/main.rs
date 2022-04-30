6cb7bea868d326c6068b6172585d333eccc974b7
Sean Toner <placeoftheway@gmail.com>
2022-04-30 14:53:47 -0400

    More docs and notebook examples. added utils

---
+++ utils/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{Subcommand, Parser, Args};
+
+
+#[derive(Debug, Parser)]
+#[clap(name = "utils")]
+#[clap(author, version, about = "Utilities for retro", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Rename(Rename)
+}
+
+#[derive(Args, Debug)]
+struct Rename {
+    #[clap(help="Substring to rename")]
+    rename: String,
+    #[clap(help="rename substring to this")]
+    to: String,
+    #[clap(long, parse(from_os_str), help="path of files to be renamed")]
+    from_path: Option<std::path::PathBuf>,
+}
+
+fn main() {
+    let parsed = Cli::parse();
+    println!("{parsed:#?}")
+}
